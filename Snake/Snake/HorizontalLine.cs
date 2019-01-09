13dff35c6ae62940f8f70ebada33b430d42a461e
ap-konovalov <sanches-zi@mail.ru>
2019-01-09 22:15:49 +0300

    Add class for draw Snake

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 
 namespace Snake
 {
-    // У насть есть класс Figure в которым мы описываем общие действия для всех
-    // фигур HorizontalLine мы решили отенсти к фигурам, это делается так class HorizontalLine : Figure.
+    // У насть есть класс Figure он называется базовый, в нем мы описываем общие действия для всех
+    // фигур HorizontalLine и  VerticalLine мы решили отенсти к фигурам, это делается так class HorizontalLine : Figure
     // HorizontalLine теперь стал наследником класса Figure, унаследовав все его свойства
 
     class HorizontalLine : Figure
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -7,17 +7,12 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+
             Point p1 = new Point(1,3,'*');
             p1.Draw();
-            
-            Point p2 = new Point(4,3,'#');
-            p2.Draw();
 
-            HorizontalLine line = new HorizontalLine(3,7,4,'$');
-            line.Drow();
-
-            VerticalLine vline = new VerticalLine(10, 2, 10, '#');
-            vline.Drow();
+            Snake snake = new Snake(p1,4,Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadLine();
         }

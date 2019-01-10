784e073131f3f5c96c7ff1148ec03d8c5b8a60ed
ap-konovalov <sanches-zi@mail.ru>
2019-01-10 22:06:31 +0300

    Snake move in different ways

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,19 +9,29 @@ namespace Snake
         static void Main(string[] args)
         {
 
+            HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+
             Point p1 = new Point(1,3,'*');
             p1.Draw();
 
             Snake snake = new Snake(p1,4,Direction.RIGHT);
             snake.Drow();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
+
+            while(true)
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+
 
         }
 
